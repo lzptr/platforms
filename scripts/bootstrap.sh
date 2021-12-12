6faba4fe7c10106c99c93085de6cca1d868dff7d
RoboGnome <26966675+RoboGnome@users.noreply.github.com>
2021-12-12 20:41:29 +0100

    Added segger download to bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -22,11 +22,12 @@ GCC_ARM="gcc-arm-none-eabi"
 GCC_VERSION="10.3-2021.10"
 GCC_PLATFORM="x86_64-linux"
 OPENOCD="openocd"
+SEGGER="JLink_Linux_V758e_x86_64.tgz"
 
 echo -e "${GREEN}Cleaning up old environment...${NC}"
 rm -rf ${TOOLCHAIN_DIR}/${GCC_ARM}-${GCC_VERSION}
 rm -rf ${TOOLCHAIN_DIR}/${OPENOCD}
-
+rm -rf ${TOOLCHAIN_DIR}/${SEGGER}
 
 echo -e "${GREEN}Getting GNU Arm Embedded Toolchain...${NC}"
 sudo mkdir -p ${TOOLCHAIN_DIR}
@@ -46,13 +47,36 @@ echo -e "${GREEN}Building openocd...${NC}"
 	&& cd ${OPENOCD}/ \
 	&& ./bootstrap \
 	&& ./configure --prefix=/usr --enable-maintainer-mode --enable-stlink --enable-ti-icdi --enable-xds110 --enable-cmsis-dap --enable-jlink --enable-armjtagew --enable-ftdi --enable-ft232r\
-	&& make)
+	&& make )
 exit_code=$?
 if [[ $exit_code -ne 0 ]]; then
     echo -e "${RED}ERROR: Building of ${OPENOCD} failed. Aborting installation.${NC}" && exit 1
+fi
+
+# Add udev rules for openocd
+sudo cp ${TOOLCHAIN_DIR}/${OPENOCD}/contrib/60-openocd.rules /etc/udev/rules.d/
+
+echo -e "${GREEN}Getting Segger JLink...${NC}"
+(cd /tmp \
+		&& wget --tries 4 --no-check-certificate --post-data="accept_license_agreement=accepted&submit=Download&nbspsoftware" -c https://www.segger.com/downloads/jlink/${SEGGER}.tgz -O ${SEGGER}.tgz \
+		&& tar xvzf ${SEGGER}.tgz -C ${TOOLCHAIN_DIR} )
+if [[ $exit_code -ne 0 ]]; then
+    echo -e "${RED}ERROR: Install of ${SEGGER} failed. Aborting installation.${NC}" && exit 1
+fi
+exit 1
+
+sudo cp ${TOOLCHAIN_DIR}/${SEGGER}/99-jlink.rules /etc/udev/rules.d/99-jlink.rule
+
+# There seems to be an issue with the udev on wsl2. Just restart and reload.
+if grep -q icrosoft /proc/version; then
+    # Add windows WSL specific options here
+	# apply wsl2 fix
+	sudo service udev restart
+	sudo udevadm control --reload
 fi
 
 # Clean up
 sudo apt-get autoremove -y \
 		&& sudo apt-get clean -y
 rm /tmp/${GCC_VERSION}.tar.bz2
+rm /tmp/${SEGGER}.tgz
